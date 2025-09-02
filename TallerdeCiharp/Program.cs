dd561c3a34744b0b1fd2085950eefa1ea99ea729
Elizabeth Caro <N00477677@upn.pe>
2025-09-02 18:10:58 -0500

    Agregar archivos de proyecto.

---
+++ TallerdeCiharp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TallerdeCsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Escribir mensaje
+            Console.WriteLine("Bienvenido a C#");
+
+            //-- Definir variables --
+            int a, b, producto;
+
+            //Entrada datos
+            Console.WriteLine("Ingrese Primer Numero");
+
+            //Guardar vaolor de la consola
+            a = int.Parse(Console.ReadLine());
+            //a = int.TryParse(Console.ReadLine()); //boolean true o face
+
+            Console.WriteLine("Ingrese segundo Numero");
+            //Guardar vaolor de la consola
+            b = Convert.ToInt32(Console.ReadLine());
+            //a = int.TryParse(Console.ReadLine()); //boolean true o face
+
+            //-- prceso --
+            producto = a * b;
+
+            //-- salida datos--
+            Console.WriteLine("El producto es: " + (producto + 2));
+            Console.WriteLine($"el producto de {a} * {b} = {producto}");
+
+
+            //Detener pantalla para mostrar resultado
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TallerdeCiharp/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("TallerdeCiharp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TallerdeCiharp")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("d9a0faf0-2952-4787-9b06-fe4b93eda570")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
